ff32620a966f4b33ce1d04b89851d8bc4771bb9c
iom00 <suri@socreative.org>
2014-02-02 21:54:50 +0900

    Update cache_model.rb
    for mongoid

--- lib/generators/letsrate/templates/cache_model.rb
+++ lib/generators/letsrate/templates/cache_model.rb
@@ -1,3 +1,11 @@
-class RatingCache < ActiveRecord::Base
+class RatingCache < Mongoid::Document
+  include Mongoid::Timestamps
+  
   belongs_to :cacheable, :polymorphic => true
+
+  field :dimension, type: String
+  field :avg, type: Float, :null => false
+  field :qty, type: Integer, :null => false
+  
+  
 end
