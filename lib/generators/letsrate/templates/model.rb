6a4deb52785b9862f52335a713e253d951bae211
iom00 <suri@socreative.org>
2014-02-02 21:55:15 +0900

    Update model.rb
    for mongoid

--- lib/generators/letsrate/templates/model.rb
+++ lib/generators/letsrate/templates/model.rb
@@ -1,6 +1,11 @@
-class Rate < ActiveRecord::Base
-  belongs_to :rater, :class_name => "<%= file_name.classify %>"
+class Rate < Mongoid::Document
+  include Mongoid::Timestamps
+  belongs_to :rater, :class_name => "<%= file_name.classify %>", index: true 
   belongs_to :rateable, :polymorphic => true
+  
+  field :dimension, type: String
+  field :stars, type: Float, :null => false
+
 
   #attr_accessible :rate, :dimension
 
